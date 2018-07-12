9bf962e24d396a807f8b3f36fa8e9e3ecd51dff5
niezhiliang <1132263176@qq.com>
2018-07-12 17:24:14 +0800

    m

---
+++ build_images.sh
@@ -0,0 +1,7 @@
+docker pull mysql:5.5.60
+
+docker pull rabbitmq:3-management
+
+docker pull redis:4.0
+
+docker pull mongo:3.2
---
+++ rm_images.sh
@@ -0,0 +1,7 @@
+docker rmi mysql:5.5.60
+
+docker rmi rabbitmq:3-management
+
+docker rmi redis:4.0
+
+docker rmi mongo:3.2
